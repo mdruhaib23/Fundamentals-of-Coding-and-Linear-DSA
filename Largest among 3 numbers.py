57bcd71de9919164b52bbdd6b157339434f4ffc2
mdruhaib23 <120497565+mdruhaib23@users.noreply.github.com>
2023-01-15 16:58:34 +0530

    Create Largest among 3 numbers.py

---
+++ Largest among 3 numbers.py
@@ -0,0 +1,10 @@
+#Write a program to print the largest of the three numbers.
+#Input:-
+#a=3, b=5, c=1
+def find_max(a,b,c):
+  if(a>b):
+    print(a)
+  elif(b>c):
+    print(b)
+  else:
+    print(C)

79b9433acf9779d8adeeac4b62535600388c55e7
mdruhaib23 <120497565+mdruhaib23@users.noreply.github.com>
2023-01-15 15:41:00 +0530

    Create Basic Coding.py

---
+++ Basic Coding.py
@@ -0,0 +1,11 @@
+#Write a program to Swap two Variables a and b  (Swapping basically means interchanging)
+#Input
+#a=3 , b=4
+#Output
+#a=4, b=3
+def swap(a,b):
+  x = a
+  a = b
+  b = x
+  print("a value is =", a)
+  print("b value is =", b)
